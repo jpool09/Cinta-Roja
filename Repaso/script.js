24015e347cbd8891d55e28ddcdf957860b29b74c
Jean Pool <jcruzcespedes72@yahoo.com>
2020-05-26 21:04:03 -0600

    Clase 26-05-20

---
+++ Repaso/script.js
@@ -0,0 +1,28 @@
+// ECMA 5
+var persona = "Pool";
+
+//ECMA 6
+let personalet = "Pool";
+const personaConst = "Pool";
+personalet = "Jean";
+
+/* Tipos de datos para las variables
+String
+Number
+Boolean
+*/
+function nombreFuncion(parametro) {}
+
+function nombreFuncion(parametro) {
+  return "Algo";
+}
+
+let perro = {
+  nombre: "Ayudante de Santa",
+  color: "café",
+  noPatas: 4,
+  tieneDueno: true,
+  ladrar(aQuien) {
+    console.log(`guao guao, ${aQuien}! sal de mi casa!`);
+  },
+};
