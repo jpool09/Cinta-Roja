853a98fff91bee3fa57192258d9740c2702f833a
Jean Pool <jcruzcespedes72@yahoo.com>
2020-06-14 18:46:40 -0600

    Ejemplo react

--- peticiones/index.js
+++ peticiones/index.js
@@ -1,1 +1,9 @@
-const request = require("request");
+const request = require("request"); //Se refiere a una libreria que esta en package.json
+
+function recibirData(error, response, body) {
+  console.log(error);
+  console.log(response);
+  console.log(body);
+}
+
+request("https://pokeapi.co/api/v2/pokemon/ditto/", recibirData);
